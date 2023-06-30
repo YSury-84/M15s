9c6bf4038816c807b9ca540d8e53d47f1e55ef0a
YSury-84 <Yury.Kolchurin@evraz.com>
2023-06-30 14:56:17 +0700

    Выполнено задание Модуля 15

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            //Реализация #1
+            List<string> linqtdata = new List<string>();
+            var res = from students in classes from student in students.Students select student;
+            linqtdata.AddRange(res);
+            return linqtdata.ToArray();
+            //Реализация #1 (не вызывается, но рабочая)
+            List<string> listdata = new List<string>();
+            foreach (var student in classes) {listdata.AddRange(student.Students);}
+            return listdata.ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
